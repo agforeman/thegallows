c5e9ebda376276210b3d97bbd9b9d132af827740
justinshapiro <justin8shapiro@yahoo.com>
2016-04-05 12:14:16 -0600

    Project ready to work on.

---
+++ app/src/main/java/csci3320/thegallows/WordBank.java
@@ -0,0 +1,18 @@
+package csci3320.thegallows;
+
+public class WordBank {
+
+    private String difficulty;
+    private String[] word_bank;
+
+    public WordBank(String _difficulty) {
+        this.difficulty = _difficulty;
+        // importWordBank(); //import word bank file of selected difficulty
+    }
+
+    // public String getWord() {} // data type of word String or char array?
+                                  // randomly generate index wordBank array
+
+    // private importWordBank() {} // import words into global string array
+
+}
