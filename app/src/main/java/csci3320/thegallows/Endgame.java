66e69af53182ea4b101e65fb931f655103355539
tyhhytfirebird9 <alexander.foreman@ucdenver.edu>
2016-04-13 14:36:22 -0600

    Start Screen, and Endgame activity and layouts mostly complete. Still need the image files, and will play around with changing button/background colors in the next few days. Also want to start implementing horizontal layouts for these activities. The Endgame activity still needs logic for determining win/lose

--- app/src/main/java/csci3320/thegallows/Endgame.java
+++ app/src/main/java/csci3320/thegallows/Endgame.java
@@ -2,6 +2,9 @@ package csci3320.thegallows;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class Endgame extends Activity {
 
@@ -9,5 +12,29 @@ public class Endgame extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_endgame);
+
+        final Button buttonPlayAgain = (Button) findViewById(R.id.play_again_button);
+        final Button buttonChangeDifficulty = (Button) findViewById(R.id.change_difficulty_button);
+
+        final Intent incomingIntent = new Intent(getIntent());
+        final String difficulty = incomingIntent.getStringExtra("Difficulty");
+
+        buttonPlayAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent replayIntent = new Intent(v.getContext(), Gameplay.class);
+                replayIntent.putExtra("Difficulty", difficulty);
+                startActivity(replayIntent);
+            }
+        });
+
+        buttonChangeDifficulty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeDifficultyIntent = new Intent(v.getContext(), StartScreen.class);
+                startActivity(changeDifficultyIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/csci3320/thegallows/StartScreen.java
+++ app/src/main/java/csci3320/thegallows/StartScreen.java
@@ -1,7 +1,11 @@
 package csci3320.thegallows;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class StartScreen extends Activity {
 
@@ -9,5 +13,44 @@ public class StartScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startscreen);
+
+        final Button buttonEasy = (Button) findViewById(R.id.easy_button);
+        final Button buttonMedium = (Button) findViewById(R.id.medium_button);
+        final Button buttonHard = (Button) findViewById(R.id.hard_button);
+        final Button buttonFreeplay = (Button) findViewById(R.id.freeplay_button);
+
+        final Intent launchGame = new Intent(this, Gameplay.class);
+
+        buttonEasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchGame.putExtra("Difficulty","EASY");
+                startActivity(launchGame);
+            }
+        });
+
+        buttonMedium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchGame.putExtra("Difficulty","MEDIUM");
+                startActivity(launchGame);
+            }
+        });
+
+        buttonHard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchGame.putExtra("Difficulty","HARD");
+                startActivity(launchGame);
+            }
+        });
+
+        buttonFreeplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                launchGame.putExtra("Difficulty","FREEPLAY");
+                startActivity(launchGame);
+            }
+        });
     }
 }
